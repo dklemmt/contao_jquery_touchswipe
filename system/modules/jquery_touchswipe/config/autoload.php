30f2f2aaffc15accb54ea623d42c15a7a8a3dafe
Dirk Klemmt <dirch@quantentunnel.de>
2012-10-11 17:42:26 +0200

    initial import

---
+++ system/modules/jquery_touchswipe/config/autoload.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ * 
+ * Copyright (C) 2005-2012 Leo Feyer
+ * 
+ * @package   jquery_touchswipe 
+ * @author    Dirk Klemmt 
+ * @license   GPL 
+ * @copyright Dirk Klemmt 2012 
+ */
+
+/**
+ * Register the templates
+ */
+TemplateLoader::addFiles(array
+(
+	'j_touchswipe' => 'system/modules/jquery_touchswipe/templates',
+));
---
+++ system/modules/jquery_touchswipe/languages/de/modules.php
@@ -0,0 +1,22 @@
+<?php 
+
+/**
+ * Contao Open Source CMS
+ * 
+ * Copyright (C) 2005-2012 Leo Feyer
+ * 
+ * @package   jquery_touchswipe 
+ * @author    Dirk Klemmt 
+ * @license   GPL 
+ * @copyright Dirk Klemmt 2012 
+ */
+
+/**
+ * Back end modules
+ */
+$GLOBALS['TL_LANG']['MOD']['jquery_touchswipe'] = array('jQuery TouchSwipe Plugin', '');
+
+/**
+ * Front end modules
+ */
+$GLOBALS['TL_LANG']['FMD']['jquery_touchswipe'] = array('jQuery TouchSwipe Plugin', '');
