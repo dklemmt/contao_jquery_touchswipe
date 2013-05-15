0e39b5fab50b577c8792c2f7740576b02a5c233b
dklemmt <dirch@quantentunnel.de>
2013-05-15 14:26:26 +0200

    updated script to version 1.6.4

--- system/modules/jquery_touchswipe/config/autoload.php
+++ system/modules/jquery_touchswipe/config/autoload.php
@@ -3,12 +3,12 @@
 /**
  * Contao Open Source CMS
  * 
- * Copyright (C) 2005-2012 Leo Feyer
+ * Copyright (C) 2005-2013 Leo Feyer
  * 
- * @package   jquery_touchswipe 
- * @author    Dirk Klemmt 
- * @license   GPL 
- * @copyright Dirk Klemmt 2012 
+ * @package   jquery_touchswipe
+ * @author    Dirk Klemmt
+ * @license   GPL
+ * @copyright Dirk Klemmt 2012-2013
  */
 
 /**
--- system/modules/jquery_touchswipe/languages/de/modules.php
+++ system/modules/jquery_touchswipe/languages/de/modules.php
@@ -3,20 +3,16 @@
 /**
  * Contao Open Source CMS
  * 
- * Copyright (C) 2005-2012 Leo Feyer
+ * Copyright (C) 2005-2013 Leo Feyer
  * 
- * @package   jquery_touchswipe 
- * @author    Dirk Klemmt 
- * @license   GPL 
- * @copyright Dirk Klemmt 2012 
+ * @package   jquery_touchswipe
+ * @author    Dirk Klemmt
+ * @license   GPL
+ * @copyright Dirk Klemmt 2012-2013
  */
+
 
 /**
  * Back end modules
  */
-$GLOBALS['TL_LANG']['MOD']['jquery_touchswipe'] = array('jQuery TouchSwipe Plugin', '');
-
-/**
- * Front end modules
- */
-$GLOBALS['TL_LANG']['FMD']['jquery_touchswipe'] = array('jQuery TouchSwipe Plugin', '');
+$GLOBALS['TL_LANG']['MOD']['jquery_touchswipe'][0] = 'jQuery TouchSwipe Plugin';
